c076b445642a352855105ee49a74c69c73622d7f
Ben Humphrey <ben.humphrey1985@gmail.com>
2015-10-22 21:06:39 +0100

    adding eicar to dll

--- InjectMe/dllmain.cpp
+++ InjectMe/dllmain.cpp
@@ -6,10 +6,33 @@ BOOL APIENTRY DllMain( HMODULE hModule,
                        LPVOID lpReserved
 					 )
 {
+	char *eicar = "X5O!P%@AP[4\\PZX54(P^)7CC)7}$EICAR-STANDARD-ANTIVIRUS-TEST-FILE!$H+H*";
+	HANDLE hFile = NULL;
+	BOOL bErrorFlag;
+
 	switch (ul_reason_for_call)
 	{
 	case DLL_PROCESS_ATTACH:
 		OutputDebugString(TEXT("InjectMe: Dll attached"));
+
+		hFile = CreateFile(L"eicar.com", GENERIC_WRITE, 0, NULL, CREATE_NEW, FILE_ATTRIBUTE_NORMAL, NULL);
+
+		if (hFile == INVALID_HANDLE_VALUE)
+		{
+			MessageBox(NULL, L"Invalid file handle!", L"Moo", 0);
+			break;
+		}
+
+		bErrorFlag = WriteFile(hFile, eicar, strlen(eicar), NULL, NULL);
+
+		if (!bErrorFlag)
+		{
+			MessageBox(NULL, L"WriteFile failed!", L"Moo", 0);
+			break;
+		}
+
+		CloseHandle(hFile);
+
 		MessageBox(NULL, L"InjectMe: Dll attached", L"Moo", 0);
 		break;
 	case DLL_THREAD_ATTACH:
